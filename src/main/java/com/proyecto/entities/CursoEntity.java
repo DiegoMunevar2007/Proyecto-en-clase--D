8590b51ba1bd5e12ddcf4451f5d79b317bdc2c25
Manu_Love <107737902+mloveral@users.noreply.github.com>
2025-03-03 15:10:43 -0500

    mainzaso

--- src/main/java/com/proyecto/entities/CursoEntity.java
+++ src/main/java/com/proyecto/entities/CursoEntity.java
@@ -1,4 +1,4 @@
-package com.proyecto.entities;
+|package com.proyecto.entities;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -6,6 +6,7 @@ import java.util.List;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
+import uk.co.jemos.podam.common.PodamExclude;
 
 @Data
 @Entity
--- src/main/java/com/proyecto/entities/EstudianteEntity.java
+++ src/main/java/com/proyecto/entities/EstudianteEntity.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.Data;
+import uk.co.jemos.podam.common.PodamExclude;
 
 @Data
 @Entity
