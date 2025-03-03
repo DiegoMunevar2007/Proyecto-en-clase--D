a109d93e7f9cc9febc899b5fa17f0123fe9a8f33
Manu_Love <107737902+mloveral@users.noreply.github.com>
2025-03-03 15:02:00 -0500

    estudiantes entity creado

---
+++ src/main/java/com/proyecto/entities/EstudianteEntity.java
@@ -0,0 +1,27 @@
+package com.proyecto.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+
+@Data
+@Entity
+public class EstudianteEntity extends BaseEntity{
+    private String login;
+    private String nombre;
+    private String codigo;
+
+    // Relaciones
+    @PodamExclude
+    @OneToOne
+    private ProgramaEntity programa;
+
+    @PodamExclude
+    @OneToMany
+    private List<InscripcionEntity> inscripciones = new ArrayList<InscripcionEntity>();
+
+}
