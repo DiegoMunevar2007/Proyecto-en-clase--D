7a57e95e107243870c76f37cd0964ad7362c0aa4
Manu_Love <107737902+mloveral@users.noreply.github.com>
2025-03-03 15:11:12 -0500

    :)

--- src/main/java/com/proyecto/entities/CursoEntity.java
+++ src/main/java/com/proyecto/entities/CursoEntity.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
 import uk.co.jemos.podam.common.PodamExclude;
