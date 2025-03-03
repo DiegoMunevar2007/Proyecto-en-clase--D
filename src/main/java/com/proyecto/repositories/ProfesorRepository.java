ce0aa6db2c848122ea21abdc2b50f666536be668
Diego Alejandro Munévar Pérez <d.munevar@uniandes.edu.co>
2025-03-03 15:21:20 -0500

    Repositoriessssss

---
+++ src/main/java/com/proyecto/repositories/ProfesorRepository.java
@@ -0,0 +1,9 @@
+package com.proyecto.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.proyecto.entities.ProfesorEntity;
+
+public interface ProfesorRepository extends JpaRepository<ProfesorEntity,Long> {
+
+}
